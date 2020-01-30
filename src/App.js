d58803c5c9aef008245b05ddfd19062fc52089f2
JuanValdez <juantvaldez85@gmail.com>
2020-01-30 16:52:44 -0500

    feat: grid of heroes, randomized hero card.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect} from 'react';
 import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid';
 import Header from './Header'
 import './App.css';
 
-
+const randomCard = Math.floor(Math.random() * Math.floor(20));
+console.log(randomCard)
 const App = () => {
   
-  const [marvelData, setData] = useState(null)
-  const marvelNullChecker = marvelData && marvelData.data;
+  const [marvelData, setData] = useState(null);
+  
+  
+  /*const marvelNullChecker = marvelData && marvelData.data;*/
   
   const url = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=281dfdf14f67bfaefa6852a403b1c138&hash=e64a7d31e8a4f7d43a0a3f3eb40115e8"
 
@@ -51,18 +55,32 @@ const App = () => {
     <div className="Wrapper-Div">
 
         <Header/>     
+
+
+        <Grid container spacing={2}>
+          
+          {marvelData.data.results.map((item, i) => (
+            
+            <Grid item>
+
+                <img className="Grid-Images" className="Border-Radius" src={item.thumbnail.path + "/standard_small" + "." + item.thumbnail.extension} />
+                {console.log(item.thumbnail.extension)}
+            </Grid>
+          ))
+
+          }
+
+
+        </Grid>
       
-      <div className="Hero-Card">
+      <div className="Hero-Card" >
 
           <div className="Hero-Info">
 
-
-          
-              <img className="Hero-Image" src={marvelData.data.results[1].thumbnail.path + "/portrait_medium" + ".jpg"} />
-              
+              <img className="Hero-Image" className="Border-Radius" src={marvelData.data.results[randomCard].thumbnail.path + "/portrait_medium" + ".jpg"} />
               
           </div>
-        
+
       </div>
     
 
